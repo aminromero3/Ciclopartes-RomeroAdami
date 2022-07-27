79354dfdecaf6fef26f6f9ae85650f0ee1cb0a82
aminromero3 <aminromeroadami@gmail.com>
2022-07-26 23:33:05 -0300

    2do desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+function NavBar(){
+    return (
+        <nav>
+            <div>
+                <h1>Ciclopartes</h1>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Productos</a></li>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Contacto</a></li>
+                    <li><a href="">Ayuda</a></li>
+                </ul>
+        </div>
+        </nav>
+    );
+  }
+
+export default NavBar;
