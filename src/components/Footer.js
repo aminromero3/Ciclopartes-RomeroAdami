2c666963f0f9b29079ef8e5994047d84449d691a
aminromero3 <aminromeroadami@gmail.com>
2022-08-02 23:04:47 -0300

    Desafio 4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
         <NavBar 
             type="footer"/>
         <div className='d-flex'>
-            <div class="col-md-4 d-flex">
-                    <span class="text-muted">Todos los derechos reservados &copy;2022</span>
+            <div className="col-md-4 d-flex">
+                    <span className="text-muted">Todos los derechos reservados &copy;2022</span>
             </div>
         
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex ">
-                <li class="ms-3"><a class="text-muted" href="https://www.facebook.com/" target="_blank"><img class="bi" width="45" height="45" src="./facebook.png" alt="facebook"></img></a></li>
-                <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/" target="_blank"><img class="bi" width="45" height="45" src="./instagram.png" alt="instagram"></img></a></li>
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex ">
+                <li className="ms-3"><a className="text-muted" href="https://www.facebook.com/" target="_blank"><img className="bi" width="45" height="45" src="./facebook.png" alt="facebook"></img></a></li>
+                <li className="ms-3"><a className="text-muted" href="https://www.instagram.com/" target="_blank"><img className="bi" width="45" height="45" src="./instagram.png" alt="instagram"></img></a></li>
             </ul>
         </div>
       </>
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ItemCount from "./ItemCount";
 
 function ItemListContainer({greeting}){{
     return (
         <div>
             <p>{greeting}</p>
+            <ItemCount stock="5" initial="1" />
         </div>
     )
 }}
