6de957e97bb11a5d30677c9d9e2760fbe45c4549
aminromero3 <111714536+aminromero1@users.noreply.github.com>
2022-09-16 16:14:07 -0300

    Cambie NabBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header(){
     return (
+
         <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" aria-label="Eighth navbar example">
-                <div className="container">
-                    {/* crear el logo con un link al home */}
-                    <Link to="/" className="navbar-brand">
-                        <img src="./logo2.png" className="logoHeader" alt="logo" />
-                    </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <NavBar 
-                    type="header" />
-                </div>
-            </nav>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src="./logo2.png" className="logoHeader img-fluid" alt="logo" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <NavBar type="header" />
+                </Container>
+            </Navbar>
         </header>
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CartWidget from "./CartWidget";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavBar = ({type}) => {
 
     if(type === "header"){
         return (
             <>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav nav me-auto mb-2 mb-md-0 ">
-                    <li className="nav-item"><Link to="/" className="nav-link px-2 link-dark">Home</Link></li>
-                    <li className="nav-item"><Link to="/ruta" className="nav-link px-2 link-dark">Bicletas de ruta</Link></li>
-                    <li className="nav-item"><Link to="/paseo" className="nav-link px-2 link-dark">Bicletas de paseo</Link></li>
-                    <li className="nav-item"><Link to="/nosotros" className="nav-link px-2 link-dark">Nosotros</Link></li>
-                    <li className="nav-item"><Link to="/contacto" className="nav-link px-2 link-dark">Contacto</Link></li>
-                    <li className="nav-item"><Link to="/ayuda" className="nav-link px-2 link-dark">Ayuda</Link></li>
-                </ul>
-            </div>
-
-            <CartWidget />
-        </>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="navbar-nav nav me-auto mb-2 mb-md-0">
+                        <Nav.Link href="/" className="nav-link px-2 link-dark">Home</Nav.Link>
+                        <NavDropdown title="Productos" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="/ruta" className="nav-link px-2 link-dark">Bicicletas de Ruta</NavDropdown.Item>
+                            <NavDropdown.Item href="/paseo" className="nav-link px-2 link-dark">Bicicletas de Paseo</NavDropdown.Item>
+                            <NavDropdown.Item href="/montaña" className="nav-link px-2 link-dark">Bicicletas de Montaña</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="/nosotros" className="nav-link px-2 link-dark">Nosotros</Nav.Link>
+                        <Nav.Link href="/contacto" className="nav-link px-2 link-dark">Contacto</Nav.Link>
+                        <Nav.Link href="/ayuda" className="nav-link px-2 link-dark">Ayuda</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <CartWidget />
+                </>
         )
     }else{
         return (
