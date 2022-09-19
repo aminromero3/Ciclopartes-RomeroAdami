5fce844cd8ce7fe8fff560b30619e9c067f8bdb9
aminromero3 <111714536+aminromero1@users.noreply.github.com>
2022-09-19 19:57:41 -0300

    modifique Checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,77 +1,119 @@
 import React from 'react'
 import { useCarrito } from "./CartContext"
+import { useState } from "react";
+import { db } from "./firebase"
+import { collection, addDoc , serverTimestamp } from "firebase/firestore";
 
 function Checkout() {
 
-    const { 
-        carrito, 
-        cantidad,
-        nombre, 
-        apellido, 
-        email, 
-        telefono, 
-        precioTotal,
-        handleChangeNombre, 
-        handleChangeApellido, 
-        handleChangeEmail, 
-        handleChangeTelefono, 
-        handleConfirm,
-    } = useCarrito();
+    const { carrito, cantidad,precioTotal} = useCarrito();
 
-  return (
-    <div className='containerForm'>
-        <div className='row g-5'>
-            {/* cart */}
-            <div className="col-md-5 col-lg-4 order-md-last">
-                <h4 className="d-flex justify-content-between align-items-center mb-3">
-                    <span className="text-primary">Tu Carrito</span>
-                    <span className="badge bg-primary rounded-pill">{cantidad}</span>
-                </h4>
-                <ul className="list-group mb-3">
-                    {carrito.map((item) => {
-                        return (
-                            <li key={item} className="list-group-item d-flex justify-content-between lh-sm" >
-                                <img className='imageCart' src={item.image} alt={item.title} />
-                                <div>  
-                                    <h6 className="my-0">{item.title}</h6>
-                                    <small className="text-muted">{item.description}</small>
-                                </div>
-                                <span className="text-muted">${item.price}</span>
-                            </li>
-                        )
-                    })}
-                <li className="list-group-item d-flex justify-content-between">
-                    <span>Total:</span>
-                    <strong>${precioTotal()}</strong>
-                </li>
-                </ul>
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const nombreCompleto = `${nombre} ${apellido}`;
+
+    const handleChangeNombre = (e) => {
+        e.preventDefault()
+        const input = e.target
+        const value = input.value
+        setNombre(value)
+        console.log(nombre)
+    }
+
+    const handleChangeApellido = (e) => {
+        const input = e.target
+        const value = input.value
+        setApellido(value)
+        console.log(apellido)
+    }
+
+    const handleChangeEmail = (e) => {
+        const input = e.target
+        const value = input.value
+        setEmail(value)
+        console.log(email)
+    }
+
+    const handleChangeTelefono = (e) => {
+        const input = e.target
+        const value = input.value
+        setTelefono(value)
+    }
+
+    const handleConfirm = () => {
+        const orden= {
+            items: carrito,
+            total : precioTotal(),
+            buyer : {
+                name : nombreCompleto,
+                phone : telefono,
+                email : email,
+            },
+            date : serverTimestamp()
+        }
+
+        const ordersCollection = collection(db, "orders")
+        const consulta = addDoc(ordersCollection, orden)
+
+        consulta
+            .then((res)=>{
+            })
+            .catch(error => {
+            })
+    }
+    
+    return (
+        <div className='containerForm'>
+            <div className='row g-5 '>
+                <div className="col-md-5 col-lg-4 order-md-last">
+                    <h4 className="d-flex justify-content-between align-items-center mb-3">
+                        <span className="text">Tu Carrito</span>
+                        <span className="badge bg-primary rounded-pill">{cantidad}</span>
+                    </h4>
+                    <ul className="list-group mb-3">
+                        {carrito.map((item) => {
+                            return (
+                                <li key={item} className="list-group-item d-flex justify-content-between lh-sm" >
+                                    <img className='imageCart' src={item.image} alt={item.title} />
+                                    <div>  
+                                        <h6 className="my-0">{item.title}</h6>
+                                        
+                                    </div>
+                                    <span className="text-muted">${item.price}</span>
+                                </li>
+                            )
+                        })}
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span>Total:</span>
+                        <strong>${precioTotal()}</strong>
+                    </li>
+                    </ul>
+                </div>
+                <div className='col-md-6 col-lg-7'>                    
+                        <div className='row g-3'>
+                            <div className="form-group col-sm-6">
+                                <label className='form-label'>Nombre</label>
+                                <input type="text" className="form-control" id="nombre" aria-describedby="nombre" onChange={handleChangeNombre} value={nombre} placeholder="Nombre" required/>
+                            </div>
+                            <div className="form-group col-sm-6">
+                                <label className='form-label'>Apellido</label>
+                                <input type="text" className="form-control" id="apellido" aria-describedby="apellido" onChange={handleChangeApellido} value={apellido} placeholder="Apellido" required/>
+                            </div>
+                            <div className="form-group col-12">
+                                <label className='form-label'>Email</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="email" onChange={handleChangeEmail} value={email} placeholder="Email" required/>
+                            </div>
+                            <div className="form-group col-12">
+                                <label className='form-label'>Telefono</label>
+                                <input type="text" className="form-control" id="telefono" aria-describedby="telefono" onChange={handleChangeTelefono} value={telefono} placeholder="Nombre" required/>
+                            </div>                       
+                            <button className=" checkoutButton" onClick={handleConfirm}>Finalizar compra</button>
+                        </div>
+                </div>
             </div>
-            {/* form */}
-            <div className='col-md-7 col-lg-8'>
-                <form className='needs-validation' noValidate>
-                    <div className='row g-3'>
-                        <div className="form-group col-sm-6">
-                            <label className='form-label'>Nombre</label>
-                            <input type="text" className="form-control" id="nombre" aria-describedby="emailHelp" onChange={handleChangeNombre} value={nombre} placeholder="Nombre" required/>
-                        </div>
-                        <div className="form-group col-sm-6">
-                            <label className='form-label'>Apellido</label>
-                            <input type="text" className="form-control" id="apellido" aria-describedby="emailHelp" onChange={handleChangeApellido} value={apellido} placeholder="Apellido" required/>
-                        </div>
-                        <div className="form-group col-12">
-                            <label className='form-label'>Email</label>
-                            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={handleChangeEmail} value={email} placeholder="Email" required/>
-                        </div>
-                        <div className="form-group col-12">
-                            <label className='form-label'>Telefono</label>
-                            <input type="text" className="form-control" id="telefono" aria-describedby="emailHelp" onChange={handleChangeTelefono} value={telefono} placeholder="Nombre" required/>
-                        </div>                       
-                        <button className="btn btn-primary" onClick={handleConfirm}>Finalizar compra</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div> 
+        </div> 
   );
 }
 
