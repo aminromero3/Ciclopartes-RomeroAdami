924b338dcad2ce2913d855822ca88b221ebcb55e
aminromero3 <111714536+aminromero1@users.noreply.github.com>
2022-09-19 18:01:17 -0300

    modifique ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,14 +3,16 @@ import Item from './Item'
 
 function ItemList({productos}) {
     
-    return (
-        <>
-            
-            {productos.map((producto) => {
-                return <Item key={producto.id} producto={producto}/>
-            })}
-            
-        </>
+    return ( 
+            <div className="album py-5 bg-light">
+                <div className="container-sm container-md">
+                    <div className="row row-cols-2 row-cols-sm-2 row-cols-md-3 g-3">
+                        {productos.map((producto) => {
+                            return <Item key={producto.id} producto={producto}/>
+                        })}
+                    </div>
+                </div>
+            </div>
     )
 }
 
